8f0f20c85d0042b61224b0ad0899b60bf67117e7
gzile <gocezile@gmail.com>
2025-04-13 19:34:35 +0200

    Implement remove task by name

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -65,6 +65,8 @@ class TaskManager {
     // 1. Remove a task by name
     public void removeTask(String name) {
         // TODO: Implement removal logic
+        tasks.removeIf(task -> task.getName().equalsIgnoreCase(name));
+        
     }
 
     // 2. Find all completed tasks
@@ -120,7 +122,7 @@ public class SI2025Lab1Main {
         manager.addTask("Buy groceries", Priority.LOW, "Personal");
 
         // MISSING: Calls to the new methods that will be implemented
-
+        manager.removeTask("Buy groceries");
         manager.printTasks();
     }
 }
